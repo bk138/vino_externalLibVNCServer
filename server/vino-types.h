5e66682f400da815345e62fcc6df1be543e2b8be
Arnaud Maillet <arnaud.maillet@collabora.co.uk>
2009-07-17 15:51:36 +0200

    Add vino-types.h to allow multi inclusion

---
+++ server/vino-types.h
@@ -0,0 +1,13 @@
+#ifndef __VINO_TYPES_H__
+#define __VINO_TYPES_H__
+
+#include "config.h"
+
+G_BEGIN_DECLS
+
+typedef struct _VinoServer VinoServer;
+typedef struct _VinoTubeServer VinoTubeServer;
+
+G_END_DECLS
+
+#endif
